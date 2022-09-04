6a899838915a00d73b3a570aed716ea3a40fe654
AlexisVC <valente.alexis24@gmail.com>
2022-09-04 12:24:53 -0500

    Change name of dist to docs

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ const ruleToJavascript = {
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, "dist"),
+        path: path.resolve(__dirname, "docs"),
         filename: 'main.js',
     },
     resolve: {
