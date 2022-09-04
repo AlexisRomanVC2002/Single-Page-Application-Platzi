f50518e7ab85f1f8dbcffb180aa841ff8b4b7989
AlexisVC <valente.alexis24@gmail.com>
2022-09-04 12:48:02 -0500

    Add responsive disign css

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -10,7 +10,7 @@ const Character = async () => {
                 <img src="${character.image}" alt="${character.name}">
                 <h2>${character.name}</h2>
             </article>
-            <article class="Character-card Info">
+            <article class="Character-card Info" id="Info">
                 <h3>Episodes: ${character.episode.length}</h3>
                 <h3>Status: ${character.status}</h3>
                 <h3>Species: ${character.species}</h3>
